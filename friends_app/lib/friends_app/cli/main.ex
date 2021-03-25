d995b9f90daeadbed357432bb0ef976ae38b8901
Jefferson Farias <jefferson.farias7@gmail.com>
2021-03-24 23:03:31 -0300

    menu para escolha da operacao

--- friends_app/lib/friends_app/cli/main.ex
+++ friends_app/lib/friends_app/cli/main.ex
@@ -1,5 +1,6 @@
 defmodule FriendsApp.CLI.Main do
   alias Mix.Shell.IO, as: Shell
+  alias FriendsApp.CLI.{MenuChoice}
 
   def start_app do
     Shell.cmd("clear")
@@ -7,11 +8,17 @@ defmodule FriendsApp.CLI.Main do
     welcome_message()
 
     Shell.prompt("Pressione ENTER para continuar...")
+
+    starts_menu_choice()
   end
 
   defp welcome_message do
     Shell.info("============ Friends App ===========")
     Shell.info("Seja bem-vindo à sua agenda pessoal!")
     Shell.info("====================================")
+  end
+
+  defp starts_menu_choice do
+    MenuChoice.start()
   end
 end
---
+++ friends_app/lib/friends_app/cli/menu.ex
@@ -0,0 +1,4 @@
+defmodule FriendsApp.CLI.Menu do
+
+  defstruct label: nil, id: nil
+end

12856dc7ed5b59c7bd3eddfc71d0529f97f0afdc
Jefferson Farias <jefferson.farias7@gmail.com>
2021-03-27 09:23:30 -0300

    cadastrar amigo e salvar em csv

--- friends_app/lib/friends_app/db/csv.ex
+++ friends_app/lib/friends_app/db/csv.ex
@@ -1,15 +1,50 @@
 defmodule FriendsApp.DB.CSV do
   alias Mix.Shell.IO, as: Shell
   alias FriendsApp.CLI.Menu
+  alias FriendsApp.CLI.Friend
+  alias NimbleCSV.RFC4180, as: CSVParser
 
   def perform(%Menu{id: id}) do
     case id do
-      :create -> Shell.info(">>> CREATE <<<")
+      :create -> create()
       :read -> Shell.info(">>> READ <<<")
       :update -> Shell.info(">>> UPDATE <<<")
       :delete -> Shell.info(">>> DELETE <<<")
     end
+
+    FriendsApp.CLI.Menu.Choice.start()
   end
 
   def perform(_), do: :error
+
+  defp create do
+    collect_data()
+    |> Map.values()
+    |> wrap_in_list()
+    |> CSVParser.dump_to_iodata()
+    |> save_csv_file()
+  end
+
+  defp collect_data do
+    Shell.cmd("clear")
+
+    %Friend{
+      name: prompt_message("Digite o nome: "),
+      email: prompt_message("Digite o email: "),
+      phone: prompt_message("Digite o phone: ")
+    }
+  end
+
+  defp prompt_message(message) do
+    Shell.prompt(message)
+    |> String.trim()
+  end
+
+  defp wrap_in_list(list) do
+    [list]
+  end
+
+  defp save_csv_file(data) do
+    File.write!("#{File.cwd!}/friends.csv", data, [:append])
+  end
 end

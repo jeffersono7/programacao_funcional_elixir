32c7d5c23db1d386a96da2da7431fb066cff6fca
Jefferson Farias <jefferson.farias7@gmail.com>
2021-03-27 11:46:59 -0300

    adicionando capacidade de processamento paralelo com partições para gerar dados de amigos

--- friends_app/lib/mix/tasks/utils/add_fake_friends.ex
+++ friends_app/lib/mix/tasks/utils/add_fake_friends.ex
@@ -5,21 +5,55 @@ defmodule Mix.Tasks.Utils.AddFakeFriends do
 
   @shortdoc "Add Fake Friends on App"
   def run(_) do
+    time_start = Time.utc_now()
+
     Faker.start(:pt)
 
-    create_friends([], 50_000)
+    create_friends(1_500_000)
     |> CSVParser.dump_to_iodata()
     |> save_csv_file()
 
     IO.puts("Amigos cadastrados com sucesso!")
+
+    time_done = Time.utc_now()
+
+    diff = Time.diff(time_done, time_start)
+    IO.puts("\nOperação realizada em #{diff} segundos!\n")
+  end
+
+  @partitions 8
+  defp create_friends(count) do
+    case count > 100 do
+      false ->
+        create_friends([], count)
+
+      true ->
+        get_partitions(count)
+        |> Enum.map(&Task.async(fn -> create_friends(&1) end))
+        |> Enum.map(&Task.await(&1, 50_000))
+        |> Enum.reduce(fn elem, acc -> elem ++ acc end)
+    end
   end
 
   defp create_friends(lista, count) when count > 0 do
-    lista ++ [random_list_friend()] ++ create_friends(lista, count - 1)
+    ([random_list_friend()] ++ lista)
+    |> create_friends(count - 1)
   end
 
   defp create_friends(lista, _count) do
     lista
+  end
+
+  defp get_partitions(count) do
+    chunk = div(count, @partitions)
+
+    [head_partitions_with_size | tail_partitions_with_size] =
+      1..@partitions
+      |> Enum.map(fn _ -> chunk end)
+
+    resto = rem(count, @partitions)
+
+    [head_partitions_with_size + resto | tail_partitions_with_size]
   end
 
   defp random_list_friend do
